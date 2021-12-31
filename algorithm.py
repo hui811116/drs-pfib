662abb1e07a6798d2636f48fb724bba42e0dbcb1
Teng-Hui <hui811116@outlook.com>
2021-12-31 11:56:53 -0500

    adding drs ib for evaluation on infoplane

--- algorithm.py
+++ algorithm.py
@@ -17,9 +17,22 @@ def drsPF(pxy,nz,beta,convthres,maxiter,**kwargs):
 	px = np.sum(pxy,axis=1)
 	py = np.sum(pxy,axis=0)
 	pxcy = pxy / py[None,:]
+	pycx = (pxy / px[:,None]).T
 
-	pzcx= rs.rand(nz,nx)
-	pzcx /= np.sum(pzcx,axis=0)
+	#sel_idx = rs.permutation(nx)
+	# controlling the initial point
+	pzcx = np.zeros((nz,nx))
+	shuffle_zx = rs.permutation(nz)
+	for idx, item in enumerate(shuffle_zx):
+		pzcx[item,idx] = 1
+	# smoothing 
+	pzcx+= 1e-2
+	# NOTE: nz<= nx always
+	##
+	#pzcx= rs.rand(nz,nx)
+	#pzcx = pycx
+
+	pzcx /= np.sum(pzcx,axis=0) # normalization
 	pz = np.sum(pzcx*px,axis=1)
 	pzcy = pzcx @ pxcy
 
@@ -61,6 +74,68 @@ def drsPF(pxy,nz,beta,convthres,maxiter,**kwargs):
 		else:
 			pzcx = new_pzcx
 			pzcy = new_pzcy
+			pz = new_pz
+	mizx = ut.calcMI(pzcx*px[None,:])
+	mizy = ut.calcMI(pzcy*py[None,:])
+	return {'pzcx':pzcx,'niter':itcnt,'conv':conv,'IZX':mizx,'IZY':mizy}
+
+
+def drsIBType1(pxy,nz,beta,convthres,maxiter,**kwargs):
+	penalty = kwargs['penalty']
+	alpha = kwargs['relax']
+	gamma = 1/ beta
+	ss_init = kwargs['sinit']
+	ss_scale = kwargs['sscale']
+	rs = RandomState(MT19937(SeedSequence(kwargs['seed'])))
+	(nx,ny) = pxy.shape
+
+	px = np.sum(pxy,axis=1)
+	py = np.sum(pxy,axis=0)
+	pxcy = pxy / py[None,:]
+	pycx = (pxy / px[:,None]).T
+
+	#sel_idx = rs.permutation(nx)
+
+	pzcx= rs.rand(nz,nx)
+
+	pzcx /= np.sum(pzcx,axis=0)
+	pz = np.sum(pzcx*px,axis=1)
+
+	dual_z = np.zeros((nz))
+	itcnt =0
+	conv = False
+	while itcnt < maxiter:
+		itcnt += 1
+		#
+		errz = pz - np.sum(pzcx*px[None,:],axis=1)
+		dual_drs_z = dual_z - (1-alpha)*penalty*errz
+		# solve: (gamma-1)H(Z)
+		grad_z = (1-gamma)*(np.log(pz)+1) + dual_drs_z + penalty*errz
+		mean_grad_z = grad_z - np.mean(grad_z)
+		ss_z = gd.validStepSize(pz,-mean_grad_z,ss_init,ss_scale)
+		if ss_z ==0:
+			break
+		new_pz = pz - mean_grad_z*ss_z
+		
+		errz = new_pz - np.sum(pzcx*px[None,:],axis=1)
+		dual_z = dual_drs_z+ penalty*errz
+		# solve -gamma H(Z|X) + H(Z|Y)
+		pzcy = pzcx @ pxcy
+		grad_x =  (gamma*(np.log(pzcx)+1) - (np.log(pzcy)+1)@pycx -(dual_z+penalty*errz))*px[None,:]
+		mean_grad_x = grad_x-np.mean(grad_x,axis=0)
+		ss_x = gd.validStepSize(pzcx,-mean_grad_x,ss_init,ss_scale)
+		if ss_x == 0:
+			break
+		new_pzcx = pzcx - ss_x * mean_grad_x
+		
+		errz = new_pz - np.sum(new_pzcx*px[None,:],axis=1)
+		dtvz = 0.5* np.sum(np.fabs(errz),axis=0)
+		if np.all(np.array(dtvz<convthres)):
+			conv = True
+			break
+		else:
+			pzcx = new_pzcx
+			pzcy = new_pzcx @ pxcy
 			pz = new_pz
 	mizx = ut.calcMI(pzcx*px[None,:])
 	mizy = ut.calcMI(pzcy*py[None,:])
--- demo_single_pf.py
+++ demo_single_pf.py
@@ -17,7 +17,7 @@ import copy
 d_base = os.getcwd()
 
 parser = argparse.ArgumentParser()
-#parser.add_argument("-beta",type=float,help='the PF beta',default=5.0)
+parser.add_argument("-beta",type=float,help='the PF beta',default=5.0)
 parser.add_argument('-ntime',type=int,help='run how many times per beta',default=1)
 parser.add_argument('-penalty',type=float,help='penalty coefficient',default=1024.0)
 parser.add_argument('-relax',type=float,help='Relaxation parameter for DRS',default=1.0)
@@ -26,9 +26,9 @@ parser.add_argument('-sinit',type=float,help='initial step size',default=1e-3)
 parser.add_argument('-sscale',type=float,help='Scaling of step size',default=0.25)
 parser.add_argument('-maxiter',type=int,help='Maximum number of iterations',default=40000)
 parser.add_argument('-seed',type=int,help='Random seed for reproduction',default=None)
-parser.add_argument('-minbeta',type=float,help='the minimum beta',default=1.0)
-parser.add_argument('-maxbeta',type=float,help='the maximum beta',default=10.0)
-parser.add_argument('-numbeta',type=float,help='beta geometric space',default=16)
+#parser.add_argument('-minbeta',type=float,help='the minimum beta',default=1.0)
+#parser.add_argument('-maxbeta',type=float,help='the maximum beta',default=10.0)
+#parser.add_argument('-numbeta',type=float,help='beta geometric space',default=16)
 
 
 
@@ -36,7 +36,7 @@ parser.add_argument('-numbeta',type=float,help='beta geometric space',default=16
 args = parser.parse_args()
 argdict = vars(args)
 
-d_beta_range = np.geomspace(args.minbeta,args.maxbeta,num=args.numbeta)
+#d_beta_range = np.geomspace(args.minbeta,args.maxbeta,num=args.numbeta)
 #data = dt.synMy()
 #data = dt.uciHeart()
 data = dt.uciHeartFail()
@@ -46,29 +46,32 @@ def runAlg(nz,beta,thres,maxiter,**kwargs):
 	return algout
 
 
-result_dict= {}
+#result_dict= {}
 nz = data['ny']
 sinit = copy.copy(argdict['sinit'])
 while nz >=2:
 	argdict['sinit'] = sinit
-	for beta in d_beta_range:
-		conv_cnt = 0
-		for nn in range(args.ntime):
-			print('\rProgress: beta={:.2f}, run={:>5}/{:>5}, nz={:>3}, ss_init={:8.2e}'.format(beta,nn,args.ntime,nz,argdict['sinit']),end='',flush=True)
-			output = runAlg(nz,beta,**argdict)
-			#print('{nidx:<3} run: IZX={IZX:>10.4f}, IZY={IZY:>10.4f}, niter={niter:>10}, converge:{conv:>5}'.format(**{'nidx':nn,**output}))
-			conv_cnt += int(output['conv'])
-			if output['conv']:
-				izx_str = '{:.2f}'.format(output['IZX'])
-				if not result_dict.get(izx_str,False):
-					result_dict[izx_str] = 9999
-				if result_dict[izx_str] >output['IZY']:
-					result_dict[izx_str] = output['IZY']
-		status_tex = 'beta:{:.2f} complete. conv_rate:{:8.4f}, sinit:{:8.4f}'.format(beta,conv_cnt/args.ntime,argdict['sinit'])
-		print('\r{:<200}'.format(status_tex),end='\r',flush=True)
-		argdict['sinit'] *= 0.9
+	#for beta in d_beta_range:
+	beta= args.beta
+	#conv_cnt = 0
+	for nn in range(args.ntime):
+		#print('\rProgress: beta={:.2f}, run={:>5}/{:>5}, nz={:>3}, ss_init={:8.2e}'.format(beta,nn,args.ntime,nz,argdict['sinit']),end='',flush=True)
+		output = runAlg(nz,beta,**argdict)
+		print('{nidx:<3} run: IZX={IZX:>10.4f}, IZY={IZY:>10.4f}, niter={niter:>10}, converge:{conv:>5}'.format(**{'nidx':nn,**output}))
+		#conv_cnt += int(output['conv'])
+		'''
+		if output['conv']:
+			izx_str = '{:.2f}'.format(output['IZX'])
+			if not result_dict.get(izx_str,False):
+				result_dict[izx_str] = 9999
+			if result_dict[izx_str] >output['IZY']:
+				result_dict[izx_str] = output['IZY']
+		'''
+	#status_tex = 'beta:{:.2f} complete. conv_rate:{:8.4f}, sinit:{:8.4f}'.format(beta,conv_cnt/args.ntime,argdict['sinit'])
+	#print('\r{:<200}'.format(status_tex),end='\r',flush=True)
+	argdict['sinit'] *= 0.9
 	nz -= 1
-print(' '*200+'\r',end='',flush=True)
+#print(' '*200+'\r',end='',flush=True)
 
 '''
 # run the corresponding IB 
@@ -95,11 +98,11 @@ for beta in d_beta_range:
 print(' '*200+'\r',end='',flush=True)
 '''
 
-dataout = []
-for k,v in result_dict.items():
-	izx_num = float(k)
-	dataout.append([izx_num,v])
-datanpy = np.array(dataout)
+#dataout = []
+#for k,v in result_dict.items():
+#	izx_num = float(k)
+#	dataout.append([izx_num,v])
+#datanpy = np.array(dataout)
 '''
 dataib = []
 for k,v in ib_res_dict.items():
@@ -107,15 +110,15 @@ for k,v in ib_res_dict.items():
 	dataib.append([izx_num,v])
 dataib = np.array(dataib)
 '''
-plt.scatter(datanpy[:,0],datanpy[:,1],marker="^",c='r')
+#plt.scatter(datanpy[:,0],datanpy[:,1],marker="^",c='r')
 #plt.scatter(dataib[:,0],dataib[:,1],marker="+",c="b")
-plt.show()
+#plt.show()
 
 # save mat
-savemat_name = 'heartfail_drspv_r_{}_c_{}_si_{:4.2e}'.format(args.relax,int(args.penalty),sinit)
+#savemat_name = 'heartfail_drspv_r_{}_c_{}_si_{:4.2e}'.format(args.relax,int(args.penalty),sinit)
 #savemat_name = 'heart_drspv_r_{}_c_{}_si_{:4.2e}'.format(args.relax,int(args.penalty),sinit)
-outmat_name = savemat_name.replace('.',"") + '.mat'
-save_location = os.path.join(d_base,outmat_name)
-savemat(save_location,{'relax':args.relax,'penalty':args.penalty,'sinit':args.sinit,'infoplane':datanpy,})
+#outmat_name = savemat_name.replace('.',"") + '.mat'
+#save_location = os.path.join(d_base,outmat_name)
+#savemat(save_location,{'relax':args.relax,'penalty':args.penalty,'sinit':args.sinit,'infoplane':datanpy,})
 
-print('simulation complete, saving results to: {}'.format(save_location))
+#print('simulation complete, saving results to: {}'.format(save_location))
